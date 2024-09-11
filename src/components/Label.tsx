7b572c4d57d94938190741408bf9a3d03f173336
hsw824 <ghdtjddn0824@naver.com>
2024-09-11 19:19:03 +0900

    label 컴포넌트 임시 asChild추가

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -8,8 +8,10 @@ interface ComponentTypes {
 }
 
 //TODO: asChild 추가하고, slot 컴포넌트 먼저 구성(const component = asChild ? 'label' : Slot)
-const LabelRoot = ({ htmlFor, children, className }: ComponentTypes) => {
-  return (
+const LabelRoot = ({ asChild = false, htmlFor, children, className }: ComponentTypes) => {
+  return asChild ? (
+    <div>{children}</div>
+  ) : (
     <label className={className} htmlFor={htmlFor}>
       {children}
     </label>
