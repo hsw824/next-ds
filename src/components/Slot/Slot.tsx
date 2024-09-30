5e061be3ac58041d4c8995437d30fd8aa784f08e
hsw824 <ghdtjddn0824@naver.com>
2024-09-30 19:47:41 +0900

    feat: slot props, ref 병합 추가

--- src/components/Slot/Slot.tsx
+++ src/components/Slot/Slot.tsx
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import Slottable, { SlottableProps } from './Slottable';
-
-interface SlotProps extends React.HTMLAttributes<HTMLElement> {
-  children: React.ReactNode;
-}
 
 export type AsChildProps<T> = T & {
   asChild?: boolean;
 };
+interface SlotProps extends React.HTMLAttributes<HTMLElement> {
+  children: React.ReactNode;
+}
 
-const Slot = ({ children, ...props }: SlotProps) => {
+type PossibleRef<T> = React.Ref<T> | undefined;
+
+type AnyProps = Record<string, any>;
+
+function mergeProps(slotProps: AnyProps, childProps: AnyProps) {
+  const overrideProps = { ...childProps };
+
+  for (const propName in childProps) {
+    const slotPropValue = slotProps[propName];
+    const childPropValue = childProps[propName];
+
+    const isHandler = /^on[A-Z]/.test(propName);
+
+    if (isHandler) {
+      if (slotPropValue && childPropValue) {
+        overrideProps[propName] = (...args: unknown[]) => {
+          childPropValue(...args);
+          slotPropValue(...args);
+        };
+      } else if (slotPropValue) {
+        overrideProps[propName] = slotPropValue;
+      }
+    } else if (propName === 'style') {
+      overrideProps[propName] = { ...slotPropValue, ...childPropValue };
+    } else if (propName === 'className') {
+      overrideProps[propName] = [slotPropValue, childPropValue].filter(Boolean).join(' ');
+    }
+  }
+  return { ...slotProps, ...overrideProps };
+}
+
+function setRef<T>(ref: PossibleRef<T>, value: T) {
+  if (typeof ref === 'function') {
+    ref(value);
+  } else if (ref !== null && ref !== undefined) {
+    (ref as React.MutableRefObject<T>).current = value;
+  }
+}
+
+function composeRefs<T>(...refs: PossibleRef<T>[]) {
+  return (node: T) => refs.forEach((ref) => setRef(ref, node));
+}
+
+function getElementRef(element: React.ReactElement) {
+  // React <=18 in DEV
+  let getter = Object.getOwnPropertyDescriptor(element.props, 'ref')?.get;
+  let mayWarn = getter && 'isReactWarning' in getter && getter.isReactWarning;
+  if (mayWarn) {
+    return (element as any).ref;
+  }
+
+  // React 19 in DEV
+  getter = Object.getOwnPropertyDescriptor(element, 'ref')?.get;
+  mayWarn = getter && 'isReactWarning' in getter && getter.isReactWarning;
+  if (mayWarn) {
+    return element.props.ref;
+  }
+
+  // Not DEV
+  return element.props.ref || (element as any).ref;
+}
+
+const Slot = forwardRef<any, SlotProps>((props, ref) => {
+  const { children, ...slotProps } = props;
   const childrenArray = React.Children.toArray(children);
   const slottable = childrenArray.find((child) => {
     return React.isValidElement(child) && child.type === Slottable;
@@ -31,8 +93,8 @@ const Slot = ({ children, ...props }: SlotProps) => {
       ? React.cloneElement(
           newElement,
           {
-            ...props,
-            ...newElement.props,
+            ...(mergeProps(slotProps, newElement.props) as any),
+            ref: ref ? composeRefs(ref, (newElement as any).ref) : (newElement as any).ref,
           },
           newChildren,
         )
@@ -40,14 +102,15 @@ const Slot = ({ children, ...props }: SlotProps) => {
   }
 
   if (React.isValidElement(children)) {
+    const childrenRef = getElementRef(children);
     return React.cloneElement(children, {
-      ...props,
-      ...children.props,
+      ...(mergeProps(slotProps, children.props) as any),
+      ref: ref ? composeRefs(ref, childrenRef) : childrenRef,
     });
   }
 
   console.warn('Slot component should have only one React element as a child');
   return null;
-};
+});
 
 export default Slot;
