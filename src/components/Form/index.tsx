a8835859dd344b024c52a89e395ec9f69f9f92b0
hsw824 <ghdtjddn0824@naver.com>
2024-10-06 22:31:59 +0900

    Feat: Form 컴포넌트 완성

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,93 +1,196 @@
-import { createContext, useContext } from 'react';
+import { createContext, forwardRef, useContext, useState } from 'react';
 import { Primitive } from '../Primitive';
 
-// 임포트 경로 절대 경로로 바꾸기
-
-interface FormType extends React.FormHTMLAttributes<HTMLFormElement> {
+interface BaseComponentType {
   asChild?: boolean;
+}
+
+interface WithChildrenProps {
   children: React.ReactNode;
 }
 
-interface FieldType extends React.HTMLAttributes<HTMLDivElement> {
-  asChild?: boolean;
-  name: string;
-  children: React.ReactNode;
-}
-
-interface LabelType extends React.LabelHTMLAttributes<HTMLLabelElement> {
-  asChild?: boolean;
-  children: React.ReactNode;
-}
-
-interface ControlType extends React.InputHTMLAttributes<HTMLInputElement> {
-  asChild?: boolean;
+interface WithOptionalChildrenProps {
   children?: React.ReactNode;
 }
 
-interface MessageType extends React.HTMLAttributes<HTMLSpanElement> {
-  asChild?: boolean;
-  children?: React.ReactNode;
-  match: ValidityMatcher;
-}
+type FormType = BaseComponentType & WithChildrenProps & React.FormHTMLAttributes<HTMLFormElement>;
 
+type FieldType = BaseComponentType &
+  WithChildrenProps &
+  React.HTMLAttributes<HTMLDivElement> & {
+    name: string;
+  };
+
+type LabelType = BaseComponentType & WithChildrenProps & React.LabelHTMLAttributes<HTMLLabelElement>;
+
+type ControlType = BaseComponentType & WithOptionalChildrenProps & React.InputHTMLAttributes<HTMLInputElement>;
+
+type MessageType = BaseComponentType &
+  WithOptionalChildrenProps &
+  React.HTMLAttributes<HTMLSpanElement> & {
+    match: ValidityMatcher;
+  };
+
+type FormComponent = React.ForwardRefExoticComponent<FormType & React.RefAttributes<HTMLFormElement>> & {
+  Field: React.ForwardRefExoticComponent<FieldType & React.RefAttributes<HTMLDivElement>>;
+  Label: React.ForwardRefExoticComponent<LabelType & React.RefAttributes<HTMLLabelElement>>;
+  Control: React.ForwardRefExoticComponent<ControlType & React.RefAttributes<HTMLInputElement>>;
+  Message: React.ForwardRefExoticComponent<MessageType & React.RefAttributes<HTMLSpanElement>>;
+  Button: React.ForwardRefExoticComponent<ButtonType & React.RefAttributes<HTMLButtonElement>>;
+};
+
+type ButtonType = BaseComponentType & WithChildrenProps & React.ButtonHTMLAttributes<HTMLButtonElement>;
 interface FieldContextType {
   name: string;
+  validityObj: ValidityState;
+  setValidityObj: React.Dispatch<React.SetStateAction<ValidityState>>;
 }
 
-const validityMatchers = ['badInput', 'valueMissing', 'tooLong', 'tooShort', 'valid'] as const;
+const validityMatchers = [
+  'badInput',
+  'customError',
+  'patternMismatch',
+  'rangeOverflow',
+  'rangeUnderflow',
+  'stepMismatch',
+  'tooLong',
+  'tooShort',
+  'typeMismatch',
+  'valueMissing',
+  'valid',
+] as const;
 
 type ValidityMatcher = (typeof validityMatchers)[number];
 
 const FieldContext = createContext<FieldContextType | null>(null);
 
-const Form = ({ children, ...props }: FormType) => {
-  return <Primitive.form {...props}>{children}</Primitive.form>;
+const useFieldContext = () => {
+  const fieldContext = useContext(FieldContext);
+  if (!fieldContext) throw new Error('context error');
+  return fieldContext;
 };
 
-const Field = ({ children, name, ...props }: FieldType) => {
+const Form = forwardRef<HTMLFormElement, FormType>(({ children, onSubmit, ...props }, ref) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const isValid = form.checkValidity();
+
+    if (!isValid) return;
+
+    if (onSubmit) {
+      onSubmit(event);
+    }
+  };
   return (
-    <FieldContext.Provider value={{ name: name }}>
-      <Primitive.div {...props}>{children}</Primitive.div>
+    <Primitive.form ref={ref} {...props} onSubmit={handleSubmit}>
+      {children}
+    </Primitive.form>
+  );
+}) as FormComponent;
+
+const Field = forwardRef<HTMLDivElement, FieldType>(({ children, name, ...props }, ref) => {
+  const [validityObj, setValidityObj] = useState<ValidityState>({
+    badInput: false,
+    customError: false,
+    patternMismatch: false,
+    rangeOverflow: false,
+    rangeUnderflow: false,
+    stepMismatch: false,
+    tooLong: false,
+    tooShort: false,
+    typeMismatch: false,
+    valueMissing: true,
+    valid: false,
+  });
+  return (
+    <FieldContext.Provider value={{ name, validityObj, setValidityObj }}>
+      <Primitive.div ref={ref} {...props}>
+        {children}
+      </Primitive.div>
     </FieldContext.Provider>
   );
-};
+});
 
-const Label = ({ children, ...props }: LabelType) => {
-  const { name } = useContext(FieldContext) as FieldContextType;
+const Label = forwardRef<HTMLLabelElement, LabelType>(({ children, ...props }, ref) => {
+  const { name } = useFieldContext();
   return (
-    <Primitive.label {...props} htmlFor={name}>
+    <Primitive.label ref={ref} {...props} htmlFor={name}>
       {children}
     </Primitive.label>
   );
-};
-const Control = ({ ...props }: ControlType) => {
-  const { name } = useContext(FieldContext) as FieldContextType;
-  return <Primitive.input {...props} name={name} />;
-};
+});
 
-const Message = (props: MessageType) => {
+const Control = forwardRef<HTMLInputElement, ControlType>((props, ref) => {
+  const { name, setValidityObj } = useFieldContext();
+  const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+    const currentTarget = e.currentTarget;
+
+    const updateValidity = validityMatchers.reduce(
+      (prevValidityState, key) => {
+        prevValidityState[key] = currentTarget.validity[key];
+        return prevValidityState;
+      },
+      {} as Record<ValidityMatcher, boolean>,
+    );
+
+    setValidityObj((prevState) => ({
+      ...prevState,
+      ...updateValidity,
+    }));
+  };
+
+  return <Primitive.input {...props} name={name} ref={ref} onInput={handleInput} />;
+});
+
+const Message = forwardRef<HTMLSpanElement, MessageType>((props, ref) => {
   const { children, match, ...messageProps } = props;
+  const { validityObj } = useFieldContext();
 
   const DEFAULT_INVALID_MESSAGE = '유효하지 않은 값입니다.';
   const DEFAULT_BUILT_IN_MESSAGES: Record<ValidityMatcher, string | undefined> = {
     badInput: DEFAULT_INVALID_MESSAGE,
+    customError: '',
+    patternMismatch: '패턴이 맞지 않습니다.',
+    rangeOverflow: '값이 기준값보다 크면 안됩니다.',
+    rangeUnderflow: '값이 기준값보다 작으면 안됩니다.',
+    stepMismatch: '유효한 값으로 맞춰주세요.',
     tooLong: '입력값이 너무 깁니다.',
     tooShort: '입력값이 너무 짧습니다.',
     valid: undefined,
     valueMissing: '필수값 입니다.',
+    typeMismatch: '형식에 맞게 작성해 주세요.',
   };
-
-  if (match) {
+  if (match && validityObj[match]) {
     const builtMessage = DEFAULT_BUILT_IN_MESSAGES[match];
-    return <Primitive.span {...messageProps}>{children || builtMessage}</Primitive.span>;
+    return (
+      <Primitive.span ref={ref} {...messageProps}>
+        {children || builtMessage}
+      </Primitive.span>
+    );
   }
+  if (match === 'valid') {
+    return (
+      <Primitive.span ref={ref} {...messageProps}>
+        {children ? children : DEFAULT_INVALID_MESSAGE}
+      </Primitive.span>
+    );
+  }
+  return null;
+});
 
-  return <Primitive.span {...messageProps}>{children ? children : DEFAULT_INVALID_MESSAGE}</Primitive.span>;
-};
+const Button = forwardRef<HTMLButtonElement, ButtonType>(({ children, ...props }, ref) => {
+  return (
+    <Primitive.button {...props} ref={ref}>
+      {children}
+    </Primitive.button>
+  );
+});
 
 Form.Field = Field;
 Form.Label = Label;
 Form.Control = Control;
 Form.Message = Message;
+Form.Button = Button;
 
 export default Form;
