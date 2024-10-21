213790f13cf8a8d39940f1fc6bab5f90e86996ef
hsw824 <ghdtjddn0824@naver.com>
2024-10-21 22:53:00 +0900

    Fix: 충돌해결

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,7 @@
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import eslint from '@eslint/js';
-import eslint from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
-import pluginReactHook from 'eslint-plugin-react-hooks';
-
-import eslintPluginPrettier from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
-export default tseslint.config(
-  {
-    ignores: ['node_modules', 'dist'],
-  },
-  eslint.configs.recommended,
 import pluginReactHook from 'eslint-plugin-react-hooks';
 
 import eslintPluginPrettier from 'eslint-plugin-prettier';
