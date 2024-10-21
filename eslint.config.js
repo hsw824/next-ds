c783b0294738dd72d36430200c039732b5bc8863
hsw824 <ghdtjddn0824@naver.com>
2024-10-21 22:46:19 +0900

    eslint flat config 변경

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,31 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
-import pluginPrettier from 'eslint-plugin-prettier';
-import configPrettier from 'eslint-config-prettier';
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+import pluginReactHook from 'eslint-plugin-react-hooks';
+
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+export default tseslint.config(
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginPrettier.configs.flat.recommended,
-  configPrettier,
-];
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      react: pluginReact,
+      'react-hook': pluginReactHook,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      ...pluginReactHook.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react/jsx-uses-vars': 'error',
+    },
+  },
+  eslintConfigPrettier,
+);
