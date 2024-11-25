33fa981080b406296400f9eb6bd705fa8e56e445
hsw824 <ghdtjddn0824@naver.com>
2024-11-25 22:26:49 +0900

    refactor: return방식 변경

--- apps/hongcha/components/MovieList.tsx
+++ apps/hongcha/components/MovieList.tsx
@@ -1,14 +1,11 @@
 import TopRatedResult from 'models/MovieInfoClass';
 import ImageCard from 'components/ImageCard';
 
-const MovieList = ({ results }: { results: TopRatedResult[] }) => {
-  return (
-    <ul className="flex flex-nowrap  bg-black w-full h-1/2 p-3 overflow-hidden">
-      {results.map(({ id, title, posterUrl }) => {
-        return <ImageCard id={id} key={id} title={title} posterUrl={posterUrl} />;
-      })}
-    </ul>
-  );
-};
-
+const MovieList = ({ results }: { results: TopRatedResult[] }) => (
+  <ul className="flex flex-nowrap  bg-black w-full h-1/2 p-3 overflow-hidden">
+    {results.map(({ id, title, posterUrl }) => (
+      <ImageCard id={id} key={id} title={title} posterUrl={posterUrl} />
+    ))}
+  </ul>
+);
 export default MovieList;
