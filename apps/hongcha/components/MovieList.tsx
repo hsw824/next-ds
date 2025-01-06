7e84c165a6ed1f06923149961232f4f922357156
hsw824 <ghdtjddn0824@naver.com>
2025-01-06 22:16:34 +0900

    Feat: 이미지 슬라이더 기본 구성 추가

--- apps/hongcha/components/MovieList.tsx
+++ apps/hongcha/components/MovieList.tsx
@@ -1,11 +1,50 @@
 import TopRatedResult from 'models/MovieInfoClass';
 import ImageCard from 'components/ImageCard';
+import { useRef, useState, useEffect } from 'react';
 
-const MovieList = ({ results }: { results: TopRatedResult[] }) => (
-  <ul className="flex flex-nowrap  bg-black w-full h-1/2 p-3 overflow-hidden">
-    {results.map(({ id, title, posterUrl }) => (
-      <ImageCard id={id} key={id} title={title} posterUrl={posterUrl} />
-    ))}
-  </ul>
-);
+interface CarouselProps {
+  results: TopRatedResult[];
+  callApi: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const MovieList = ({ results, callApi }: CarouselProps) => {
+  const [index, setIndex] = useState(0);
+  const $ul = useRef<HTMLUListElement>(null);
+  // const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    if (!$ul.current) return;
+    $ul.current.style.transform = `translateX(-${100 * index}%)`;
+  }, [index]);
+
+  const handlePrevPage = () => {
+    const newPage = index - 1;
+    if (newPage === -1) return;
+    setIndex(newPage);
+  };
+
+  const handleNextPage = () => {
+    const newPage = index + 1;
+    if (results.length === newPage * 4) {
+      callApi((prev) => prev + 1);
+    }
+    setIndex(newPage);
+  };
+  return (
+    <div className="bg-black w-full overflow-hidden relative">
+      <button className="bg-white absolute top-0 h-full z-10" onClick={handlePrevPage}>
+        prev
+      </button>
+      <ul ref={$ul} className="flex flex-nowrap transition-transform duration-300">
+        {results.map(({ id, title, posterUrl }) => (
+          <ImageCard id={id} key={id} title={title} posterUrl={posterUrl} />
+        ))}
+      </ul>
+
+      <button className="bg-white absolute h-full top-0 right-0 z-10" onClick={handleNextPage}>
+        next
+      </button>
+    </div>
+  );
+};
 export default MovieList;
--- apps/hongcha/queries/useTopRated.ts
+++ apps/hongcha/queries/useTopRated.ts
@@ -1,22 +1,23 @@
 import MovieInfoClass from 'models/MovieInfoClass';
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { getTopRatedList } from 'apis/getTopRatedList';
 import { TMDBQueryResponseType } from 'types/responseTypes';
 
 export const useTopRated = (pageNum: number) => {
-  const {
-    isError,
-    isLoading,
-    error,
-    data: { results = [], totalPages = 0 } = {},
-  } = useQuery<TMDBQueryResponseType, Error, { results: MovieInfoClass[]; totalPages: number }>({
+  const query = useQuery<TMDBQueryResponseType, Error, { results: MovieInfoClass[]; totalPages: number }>({
     queryKey: ['topRated', pageNum],
     queryFn: () => getTopRatedList(pageNum),
     select: (data) => ({
       results: data.results.map((result) => new MovieInfoClass(result)),
       totalPages: data.totalPages,
     }),
+    placeholderData: keepPreviousData,
   });
-
-  return { isError, isLoading, results, totalPages, error };
+  return {
+    ...query,
+    data: {
+      results: query.data?.results ?? [],
+      totalPages: query.data?.totalPages ?? 0,
+    },
+  };
 };
